55c3ec00b83b5d152f7e644072e9aeba4efa17b7
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2016-12-14 14:06:39 +0100

    measurements, improvements

--- main_bot.py
+++ main_bot.py
@@ -14,43 +14,39 @@ def main():
 
     input("Press Enter to start")
 
-    i = 0.0
+    # i = 0.0
     dropped_packages = 0
-    start = time.time()
+    # start = time.time()
 
-    benchmark_start = []
-    benchmark_control = []
-    benchmark_stop = []
+    # benchmark_start = []
+    # benchmark_control = []
+    # benchmark_stop = []
 
     try:
         while True:
-            benchmark_start.append(time.time())
-            i += 1
+            # benchmark_start.append(time.time())
+            # i += 1
 
             b.send(b.getData(), "DATA")
             try:
-                try:
-                    speed_left, speed_right = b.receive("CONTROL", timeout=50).getLastCorrection()
-                except BlackLineException:
-                    b.stop().receive("GO")
-                    benchmark_start.pop()
-                    continue
+                speed_left, speed_right = b.receive("CONTROL", timeout=50).getLastCorrection()
             except TimeoutError:
                 b.zero()
                 dropped_packages += 1
-                benchmark_start.pop()
+                # benchmark_start.pop()
                 continue
             else:
-                benchmark_control.append(time.time())
+                # benchmark_control.append(time.time())
                 b.left(speed_left).right(speed_right)
 
-            benchmark_stop.append(time.time())
+            # benchmark_stop.append(time.time())
 
     except KeyboardInterrupt:
         b.stop().reset()
         # print("The average loop time was", (time.time() - start)/i * 1000, "ms")
         print(dropped_packages, "Packages where dropped because of AP not responding in time")
 
+        '''
         if not len(benchmark_start):
             return
 
@@ -77,6 +73,7 @@ def main():
                 print("Global", value, file=f)
                 print("Socket", result_socket[key], file=f)
                 print("Control", result_control[key], file=f)
+        '''
 
 if __name__ == '__main__':
     # profile.run('main()')
---
+++ meauserments/read_better_sensor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import ColorSensor
+import sys
+import time
+
+sys.path.append("..")
+
+from node.ev3con.linienverfolgung.control import BetterColorSensor
+
+try:
+    ports = sys.argv[1]
+except IndexError:
+    ports = '1234'
+
+sensors = {}
+
+for i in ports:
+    port = "in" + i
+    temp = BetterColorSensor(port=i)
+    if temp.connected:
+        temp.mode = ColorSensor.MODE_RGB_RAW
+        sensors[i] = temp
+
+
+print("running measurements...")
+
+for i in sensors:
+    port = "in" + i
+    print("Measure sensor at port", port)
+    start = time.time()
+    for cnt in range(1000):
+        r = sensors[i].grey_avg
+    end = time.time()
+    print("Sensor", port, "needed", (end-start), "ms in average")
+
+
+print("Measure all sensors together")
+
+start = time.time()
+
+for cnt in range(1000):
+    for i in sensors:
+        r = sensors[i].grey_avg
+end = time.time()
+
+print("Sensors needed", (end-start), "ms in average")
